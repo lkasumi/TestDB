6a58ca385fdecb1639787104322c6094e706d0fe
LKasumi <LKasumi@gyuhyeon-lyuui-MacBook.local>
2013-05-28 19:50:44 +0900

    Initial Commit

---
+++ TestDB/main.m
@@ -0,0 +1,18 @@
+//
+//  main.m
+//  TestDB
+//
+//  Created by LKasumi on 13. 5. 28..
+//  Copyright (c) 2013년 LKasumi. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+#import "testDBAppDelegate.h"
+
+int main(int argc, char *argv[])
+{
+    @autoreleasepool {
+        return UIApplicationMain(argc, argv, nil, NSStringFromClass([testDBAppDelegate class]));
+    }
+}
---
+++ TestDB/testDBAppDelegate.h
@@ -0,0 +1,22 @@
+//
+//  testDBAppDelegate.h
+//  TestDB
+//
+//  Created by LKasumi on 13. 5. 28..
+//  Copyright (c) 2013년 LKasumi. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface testDBAppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+@property (readonly, strong, nonatomic) NSManagedObjectContext *managedObjectContext;
+@property (readonly, strong, nonatomic) NSManagedObjectModel *managedObjectModel;
+@property (readonly, strong, nonatomic) NSPersistentStoreCoordinator *persistentStoreCoordinator;
+
+- (void)saveContext;
+- (NSURL *)applicationDocumentsDirectory;
+
+@end
